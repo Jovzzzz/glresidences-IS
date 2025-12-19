21d8b8ae6539236109b1fc990bc149a1f1cb3e2d
Jovz Adrian Martinez <jovencioadrian.martinez@lorma.edu>
2025-12-19 22:17:59 +0800

    Delete gl_residences directory

--- gl_residences/src/main/java/com/jovz/gl_residences_backend/repository/AnnouncementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jovz.gl_residences_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jovz.gl_residences_backend.model.Announcement;
-
-public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
-}
--- gl_residences/src/main/java/com/jovz/gl_residences_backend/repository/RoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.jovz.gl_residences_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jovz.gl_residences_backend.model.Room;
-
-public interface RoomRepository extends JpaRepository<Room, Long> {
-}
